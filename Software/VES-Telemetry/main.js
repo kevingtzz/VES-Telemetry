ce3348980ca92d43f83bc2664d2e25e1a02b403d
Kevin Gutierrez G <kevingtzz@MacBook-Air-de-Kevin.local>
2021-11-02 18:34:20 -0500

    Firs commit

---
+++ Software/VES-Telemetry/main.js
@@ -0,0 +1,158 @@
+const {app, BrowserWindow, Menu, MenuItem, ipcMain} = require('electron');
+const path = require('path');
+
+app.on('ready', () => {
+    create_main_window();
+});
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open
+    if (mainWindow === null) {
+      create_main_window();
+    }
+});
+
+app.on('window-all-closed', () => {
+// On macOS it is common for applications and their menu bar
+// to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+});
+
+//=============================== Windows =================================//
+
+function create_main_window() {
+    
+    mainWindow = new BrowserWindow({
+        width: 1400,
+        height: 700,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        minWidth: 800,
+        minHeight: 650,
+        title: "VES Telemetry"
+    });
+
+    const template = [
+        // { role: 'appMenu' }
+        ...(process.platform === 'darwin' ? [{
+            label: app.name,
+            submenu: [{
+                    role: 'hide'
+                },
+                {
+                    role: 'hideothers'
+                },
+                {
+                    role: 'unhide'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    role: 'quit'
+                }
+            ]
+        }] : []),
+        // { role: 'viewMenu' }
+        {
+            label: 'View',
+            submenu: [{
+                    role: 'reload'
+                },
+                {
+                    role: 'forcereload'
+                },
+                {
+                    role: 'toggledevtools'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    role: 'resetzoom'
+                },
+                {
+                    role: 'zoomin'
+                },
+                {
+                    role: 'zoomout'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    role: 'togglefullscreen'
+                }
+            ]
+        },
+        // { role: 'windowMenu' }
+        {
+            label: 'Window',
+            submenu: [{
+                    role: 'minimize'
+                },
+                {
+                    role: 'zoom'
+                },
+                ...(process.platform === 'darwin' ? [{
+                        type: 'separator'
+                    },
+                    {
+                        role: 'front'
+                    },
+                    {
+                        type: 'separator'
+                    },
+                    {
+                        role: 'window'
+                    }
+                ] : [{
+                    role: 'close'
+                }])
+            ]
+        },
+        {
+            label: 'Tools',
+            submenu: [{
+                label: 'Console',
+                click: () => {
+                    mainWindow.webContents.openDevTools();
+                }
+            }]
+        },
+        {
+            role: 'help',
+            submenu: [{
+                label: 'Learn More',
+                click: async () => {
+                    const {
+                        shell
+                    } = require('electron')
+                    await shell.openExternal('https://electronjs.org')
+                }
+            }]
+        }
+    ]
+
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+
+    // and load the index.html of the app.
+    mainWindow.loadFile(path.join(__dirname, 'src/views/main.html'));
+
+    //mainWindow.webContents.openDevTools();
+
+    // Emitido cuando la ventana es cerrada.
+    mainWindow.on('closed', () => {
+        // Elimina la referencia al objeto window, normalmente  guardarías las ventanas
+        // en un vector si tu aplicación soporta múltiples ventanas, este es el momento
+        // en el que deberías borrar el elemento correspondiente.
+        mainWindow = null;
+        // serial.set_mainWindow(mainWindow);
+    });
+
+}
